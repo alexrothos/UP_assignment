76293f60f29cd9da99223ad5227cbce8629ad729
Alex Rothos <alexrothos@gmail.com>
2023-02-10 23:04:52 +0200

    Models & Schemas

--- models/models.py
+++ models/models.py
@@ -1,9 +1,44 @@
+from app import db
+from marshmallow import Schema, fields
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-class Company():
-    pass
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
 
-class Employee():
-    pass
+    def __repr__(self):
+        return '<Company {}>'.format(self.name)
 
-class Product():
-    pass
+class CompanySchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Company
+        include_relationships = True
+        load_instance = True
+
+class Employee(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+
+    def __repr__(self):
+        return '<Employee {}>'.format(self.name)
+
+class EmployeeSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Employee
+        include_relationships = True
+        load_instance = True
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'))
+
+    def __repr__(self):
+        return '<Product {}>'.format(self.name)
+
+class ProductSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Product
+        include_relationships = True
+        load_instance = True
